d13ba31ec12497444025bb9645fad830e716bf1a
KATESPC\artif <jaryd.peters@ndsu.edu>
2025-02-12 00:31:49 -0600

    simplifies functions with struct

--- headers/cutlistOptimizer.h
+++ headers/cutlistOptimizer.h
@@ -3,6 +3,19 @@
 
 #include <stdlib.h>  // For malloc and free
 
+typedef struct 
+{
+    int *optimal_assignments;
+    int *current_assignments;
+    int *remaining_stock_space;
+    int optimal_stock_count;
+    int optimal_waste;
+    int current_stock_count;
+    int stock_length;
+    int total_pieces;
+    int *piece_sizes;
+} PackingState;
+
 typedef struct {
     int *required_pieces;
     int piece_count;
@@ -15,7 +28,7 @@ typedef struct {
     int waste;
 } CutlistResult;
 
-void find_best_packing(int *pieces, int piece_count, int stock_length, int *best_assignments, int *best_stock_used, int *best_waste, int *current_assignments, int *stock_remaining, int stock_used, int index);
+void find_best_packing(PackingState *state, int current_piece_index);
 void optimize_cutlist(CutlistInput input, CutlistResult *result);
 
 #endif // CUTLIST_OPTIMIZER_H
--- src/cutlistOptimizer.c
+++ src/cutlistOptimizer.c
@@ -4,73 +4,94 @@
 
 #include "cutlistOptimizer.h"
 
-void find_best_packing(int *pieces, int piece_count, int stock_length, int *best_assignments, int *best_stock_used, int *best_waste, int *current_assignments, int *stock_remaining, int stock_used, int index) {
-    if (index == piece_count) {
+void find_best_packing(PackingState *state, int current_piece_index) 
+{
+    if (current_piece_index == state->total_pieces) 
+    {
         // Calculate total waste
         int total_waste = 0;
-        for (int i = 0; i < stock_used; i++) {
-            total_waste += stock_remaining[i];
+        for (int stock_index = 0; stock_index < state->current_stock_count; stock_index++) 
+        {
+            total_waste += state->remaining_stock_space[stock_index];
         }
 
         // If this configuration is better, store it
-        if (total_waste < *best_waste) {
-            *best_waste = total_waste;
-            *best_stock_used = stock_used;
-            for (int i = 0; i < piece_count; i++) {
-                best_assignments[i] = current_assignments[i];
+        if (total_waste < state->optimal_waste) 
+        {
+            state->optimal_waste = total_waste;
+            state->optimal_stock_count = state->current_stock_count;
+            for (int piece_index = 0; piece_index < state->total_pieces; piece_index++) 
+            {
+                state->optimal_assignments[piece_index] = state->current_assignments[piece_index];
             }
         }
         return;
     }
 
-    int piece_size = pieces[index];
+    int current_piece_size = state->piece_sizes[current_piece_index];
 
     // Try placing this piece in an existing stock piece
-    for (int i = 0; i < stock_used; i++) {
-        if (stock_remaining[i] >= piece_size) {
-            stock_remaining[i] -= piece_size;
-            current_assignments[index] = i;
-            find_best_packing(pieces, piece_count, stock_length, best_assignments, best_stock_used, best_waste, current_assignments, stock_remaining, stock_used, index + 1);
-            stock_remaining[i] += piece_size;  // Backtrack
+    for (int stock_index = 0; stock_index < state->current_stock_count; stock_index++) 
+    {
+        if (state->remaining_stock_space[stock_index] >= current_piece_size) 
+        {
+            state->remaining_stock_space[stock_index] -= current_piece_size;
+            state->current_assignments[current_piece_index] = stock_index;
+            find_best_packing(state, current_piece_index + 1);
+            state->remaining_stock_space[stock_index] += current_piece_size;  // Backtrack
         }
     }
 
     // Try placing this piece in a new stock piece
-    stock_remaining[stock_used] = stock_length - piece_size;
-    current_assignments[index] = stock_used;
-    find_best_packing(pieces, piece_count, stock_length, best_assignments, best_stock_used, best_waste, current_assignments, stock_remaining, stock_used + 1, index + 1);
-    stock_remaining[stock_used] = 0;  // Backtrack
+    state->remaining_stock_space[state->current_stock_count] = state->stock_length - current_piece_size;
+    state->current_assignments[current_piece_index] = state->current_stock_count;
+    state->current_stock_count++;
+    find_best_packing(state, current_piece_index + 1);
+    state->current_stock_count--;
+    state->remaining_stock_space[state->current_stock_count] = 0;  // Backtrack
 }
 
-void optimize_cutlist(CutlistInput input, CutlistResult *result) {
-    int *best_assignments = (int *)malloc(input.piece_count * sizeof(int));
-    int *current_assignments = (int *)malloc(input.piece_count * sizeof(int));
-    int *stock_remaining = (int *)malloc(input.piece_count * sizeof(int));
-    int best_stock_used = input.piece_count;
-    int best_waste = INT_MAX;
+void optimize_cutlist(CutlistInput input, CutlistResult *result) 
+{
+    PackingState state;
+
+    state.total_pieces = input.piece_count;
+    state.stock_length = input.stock_length;
+    state.piece_sizes = input.required_pieces;
+    state.optimal_waste = INT_MAX;
+    state.optimal_stock_count = input.piece_count;
+    state.current_stock_count = 0;
+
+    state.optimal_assignments = (int *)malloc(input.piece_count * sizeof(int));
+    state.current_assignments = (int *)malloc(input.piece_count * sizeof(int));
+    state.remaining_stock_space = (int *)malloc(input.piece_count * sizeof(int));
 
     // Sort pieces in descending order (Largest First)
-    for (int i = 0; i < input.piece_count - 1; i++) {
-        for (int j = i + 1; j < input.piece_count; j++) {
-            if (input.required_pieces[i] < input.required_pieces[j]) {
-                int temp = input.required_pieces[i];
-                input.required_pieces[i] = input.required_pieces[j];
-                input.required_pieces[j] = temp;
+    for (int first_piece = 0; first_piece < input.piece_count - 1; first_piece++) 
+    {
+        for (int second_piece = first_piece + 1; second_piece < input.piece_count; second_piece++) 
+        {
+            if (input.required_pieces[first_piece] < input.required_pieces[second_piece]) 
+            {
+                int temp_size = input.required_pieces[first_piece];
+                input.required_pieces[first_piece] = input.required_pieces[second_piece];
+                input.required_pieces[second_piece] = temp_size;
             }
         }
     }
 
     // Start searching for the best packing
-    find_best_packing(input.required_pieces, input.piece_count, input.stock_length, best_assignments, &best_stock_used, &best_waste, current_assignments, stock_remaining, 0, 0);
+    find_best_packing(&state, 0);
 
     // Store the best result
-    result->stock_used = best_stock_used;
-    result->waste = best_waste;
-    for (int i = 0; i < input.piece_count; i++) {
-        result->assignments[i] = best_assignments[i];
+    result->stock_used = state.optimal_stock_count;
+    result->waste = state.optimal_waste;
+    for (int piece_index = 0; piece_index < input.piece_count; piece_index++) 
+    {
+        result->assignments[piece_index] = state.optimal_assignments[piece_index];
     }
 
-    free(best_assignments);
-    free(current_assignments);
-    free(stock_remaining);
+    free(state.optimal_assignments);
+    free(state.current_assignments);
+    free(state.remaining_stock_space);
 }
